f82da86a2ad5a7a834f1896b6fb8bc11f92db5b9
Tony <chentony17@gmail.com>
2020-11-17 00:36:12 -0500

    added backend

---
+++ app/controllers/api/sessions_controller.rb
@@ -0,0 +1,22 @@
+class Api::SessionsController < ApplicationController
+    
+    def create 
+        @user = User.find_by_credentials(params[:user][:email], params[:user][:password])
+        if @user 
+            login(@user)
+            render "api/users/show"
+        else
+            render json: ['email or password is invalid'], status: 401
+        end
+    end
+
+    def destroy 
+        @user = current_user
+        if @user
+            render json: {}
+            logout
+        else
+            render json: ['Invalid action'], status: 404
+        end
+    end    
+end
