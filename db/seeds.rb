0ec93c2723cc0ac423e1e5e0c013da3a468dc1f7
Tony <chentony17@gmail.com>
2020-11-19 01:48:31 -0500

    added demo seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+demo = User.create(
+    firstname: "Demo",
+    email: "demo@aa.com",
+    password_digest: BCrypt::Password.create('123456'),
+    session_token: SecureRandom.urlsafe_base64
+)
