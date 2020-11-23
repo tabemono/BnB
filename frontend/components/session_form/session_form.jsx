9820bb8011cfddc7339f12dd2d80d6e370ff015a
Tony <chentony17@gmail.com>
2020-11-23 17:58:47 -0500

    fixed modal

--- frontend/components/session_form/session_form.jsx
+++ frontend/components/session_form/session_form.jsx
@@ -1,106 +1,105 @@
-import React from 'react';
-import { withRouter } from 'react-router-dom';
-import { BsFillXCircleFill } from 'react-icons/bs';
-
+import React from "react";
+import { withRouter } from "react-router-dom";
+import { BsFillXCircleFill } from "react-icons/bs";
 
 class SessionForm extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            email: "Email",
-            password: "Password",
-            firstname: "First Name"
-        };
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
-    
-    handleSubmit(e) {
-        e.preventDefault();
-        this.props
-            .processForm(this.state)
-            .then(this.props.closeModal);
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      password: "",
+      firstname: "",
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-    update(field) {
-       return e => this.setState({
-            [field]: e.target.value
-        });
-    }
+  handleSubmit(e) {
+    e.preventDefault();
+    this.props.processForm(this.state).then(this.props.closeModal);
+  }
 
-    renderErrors() {
-        return (
-        <ul>
-            {this.props.errors.map((error, i) => (
-                <li key={`error-${i}`}>{error}</li>
-            ))}
-        </ul>
-        );
-    }
+  update(field) {
+    return (e) =>
+      this.setState({
+        [field]: e.target.value,
+      });
+  }
 
-    render() {
-        const header = this.props.formType === "Log In" ? "Log In" : "Sign Up";
-        const isSignedUp = this.props.formType === 'Sign Up';
-        const fname = () => {
-            return  (
-                                <input type='text'
-                                    value={this.state.firstname}
-                                    onChange={this.update('firstname')}
-                                    className="login-input"
-                                />
-                            
-                    )  
+  renderErrors() {
+    return (
+      <ul>
+        {this.props.errors.map((error, i) => (
+          <li key={`error-${i}`}>{error}</li>
+        ))}
+      </ul>
+    );
+  }
+
+  render() {
+    const header = this.props.formType === "Log In" ? "Log In" : "Sign Up";
+    const isSignedUp = this.props.formType === "Sign Up";
+    const fname = () => {
+      return (
+        <input
+          type="text"
+          placeholder="First Name"
+          value={this.state.firstname}
+          onChange={this.update("firstname")}
+          className="login-input"
+        />
+      );
+    };
+    return (
+      <ul>
+          <form onSubmit={this.handleSubmit} className="input-box">
+            <header className="modal-header">
+            Bikes and Bikers
+          <div onClick={this.props.closeModal} className="close-x">
+            <BsFillXCircleFill />
+          </div>
+            </header>
+            <div className="input-container">
+            {this.renderErrors()}
+            <div className="login-form">
+              {isSignedUp ? fname() : null}
+
+              <input
+                type="text"
+                placeholder="Email"
+                value={this.state.email}
+                onChange={this.update("email")}
+                className="login-input"
+              />
+
+              <input
+                type="password"
+                placeholder="Password"
+                value={this.state.password}
+                onChange={this.update("password")}
+                className="login-input"
+              />
+
+              <button
+                className="session-submit"
+                onClick={() =>
+                  this.props
+                    .login({ email: "demo@aa.com", password: "123456" })
+                    .then(this.props.closeModal)
                 }
-        return (
-          <ul>
-            <div className="login-form-container">
-              <div onClick={this.props.closeModal} className="close-x">
-                <BsFillXCircleFill />
-              </div>
-              <form onSubmit={this.handleSubmit} className="login-form-box">
-                Bikes and Bikers
-                {this.renderErrors()}
-                <div className="login-form">
-                  {isSignedUp ? fname() : null}
-
-                  <input
-                    type="text"
-                    value={this.state.email}
-                    onChange={this.update("email")}
-                    className="login-input"
-                  />
-
-                  <input
-                    type="password"
-                    value={this.state.password}
-                    onChange={this.update("password")}
-                    className="login-input"
-                  />
-
-                  <button
-                    className="session-submit"
-                    onClick={() =>
-                      this.props
-                        .login({ email: "demo@aa.com", password: "123456" })
-                        .then(this.props.closeModal)
-                    }
-                  >
-                    Demo
-                  </button>
-                  <input
-                    className="session-submit"
-                    type="submit"
-                    value={this.props.formType}
-                  />
-                </div>
-              </form>
+              >
+                Demo
+              </button>
+              <input
+                className="session-submit"
+                type="submit"
+                value={this.props.formType}
+              />
             </div>
-          </ul>
-        );
-    }
-
-
+        </div>
+          </form>
+      </ul>
+    );
+  }
 }
-
-
 
 export default withRouter(SessionForm);
