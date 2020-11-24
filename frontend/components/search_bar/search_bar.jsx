7c56bc5be4c0cde17937d987770603642f65e742
Tony <chentony17@gmail.com>
2020-11-24 12:16:05 -0500

    buggy calendar

--- frontend/components/search_bar/search_bar.jsx
+++ frontend/components/search_bar/search_bar.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { withRouter } from "react-router-dom";
-
+import { DateRangePicker } from "react-dates";
+import "react-dates/initialize";
+import "react-dates/lib/css/_datepicker.css";
 class SearchBar extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      search: "",
-    };
+    this.state = { ...this.props.query };
 
     this.handleSubmit = this.handleSubmit.bind(this);
     this.update = this.update.bind(this);
@@ -14,44 +14,67 @@ class SearchBar extends React.Component {
 
   handleSubmit(e) {
     e.preventDefault();
-    this.props
-      .fetchRides({ brand: this.state.search })
-      .then(() =>
-        this.props.history.push(`/rides/?search=${this.state.search}`)
-      );
+    const query = this.state;
+    query["startDate"] = null;
+    query["endDate"] = null;
+    this.props.runSearch(query);
+    this.props.fetchRides(query);
+    if (this.props.location.pathname !== "/rides") {
+      this.props.history.push(`/rides/?search=${this.state.search}`);
+    }
   }
-  
+
   update(field) {
-      return e => {
-          this.setState({ [field]: e.target.value });
-      }
+    return (e) => {
+      this.setState({ [field]: e.target.value });
+    };
   }
 
   render() {
-      return (
-        <div>
-          <form className="search-form" onSubmit={this.handleSubmit}>
-            <div className="search-icon-box">
-           
-                <div className="search-icon">
-                  <i className="fas fa-search fa-lg"></i>
-                </div>
-             
-            </div>
-            <input
-              className="search-form-input"
-              type="text"
-              placeholder="Anywhere in NYC"
-              value={this.state.search}
-              onChange={this.update("search")}
+    return (
+      <div className="search-wrapper">
+        <form className="search-form" onSubmit={this.handleSubmit}>
+          <div className="search-icon-box"></div>
+          <input
+            className="search-form-input"
+            type="text"
+            placeholder="Nearby Rides"
+            value={this.state.search}
+            onChange={this.update("search")}
+          />
+          <div className="calendars">
+            <label id="checkin-label">Check in</label>
+            <label id="checkout-label">Check out</label>
+            <DateRangePicker
+              startDate={this.state.startDate} // momentPropTypes.momentObj or null,
+              startDateId="search_start" // PropTypes.string.isRequired,
+              endDate={this.state.endDate} // momentPropTypes.momentObj or null,
+              endDateId="search_end" // PropTypes.string.isRequired,
+              onDatesChange={({ startDate, endDate }) =>
+                this.setState({ startDate, endDate })
+              } // PropTypes.func.isRequired,
+              focusedInput={this.state.focusedInput} // PropTypes.oneOf([START_DATE, END_DATE]) or null,
+              onFocusChange={(focusedInput) => this.setState({ focusedInput })} // PropTypes.func.isRequired,
+              startDatePlaceholderText="Add dates"
+              endDatePlaceholderText="Add dates"
+              displayFormat="MMM D"
+              noBorder={false}
+              hideKeyboardShortcutsPanel={true}
+              withPortal
+              block
+              readOnly
+              daySize={50}
             />
-            <input className="search-button" type="submit" value="Search" />
-          </form>
-        </div>
-      );
+          </div>
+          <button className="search-button">
+            <div id="search-badge">
+              <i className="fas fa-search"></i>
+            </div>
+          </button>
+        </form>
+      </div>
+    );
   }
-
 }
-
 
 export default withRouter(SearchBar);
--- frontend/components/search_bar/search_bar_container.jsx
+++ frontend/components/search_bar/search_bar_container.jsx
@@ -2,12 +2,27 @@ import { connect } from "react-redux";
 import SearchBar from "./search_bar";
 import { fetchRides } from "../../actions/ride_actions";
 import { withRouter } from "react-router-dom";
+import { runSearch } from "../../actions/search_actions";
 
-export const mdp = (dispatch) => ({
-  fetchRides: (rides) => dispatch(fetchRides(rides)),
+const mstp = state => {
+  return {
+    query: {
+      location: "",
+      brand: "",
+      style: "",
+      model: "",
+    }
+  }
+}
+
+const mdp = (dispatch) => {
+  return {
+    fetchRides: (query) => dispatch(fetchRides(query)),
+    runSearch: (query) => dispatch(runSearch(query))
+  }
     
-});
+};
 
 
 
-export default withRouter(connect(null, mdp)(SearchBar));
+export default withRouter(connect(mstp, mdp)(SearchBar));
