589432a2118273ad9e1156c3b17be760dc62f6ec
Tony <chentony17@gmail.com>
2020-11-28 18:08:03 -0500

    fix nav css

--- frontend/components/navbar/navbar.jsx
+++ frontend/components/navbar/navbar.jsx
@@ -4,15 +4,13 @@ import SearchBarContainer from "../search_bar/search_bar_container";
 import { withRouter } from "react-router-dom";
 // import DateSearch from './date_search';
 
-
 class NavBar extends React.Component {
   render() {
     const navDisplay = this.props.currentUser ? (
       <div className="outerDiv">
-        <div className='inner-div'>
-
-        <p>{this.props.currentUser.firstname}</p>
-        <button onClick={this.props.logout}>Log Out</button>
+        <div className="inner-div">
+          {this.props.currentUser.firstname}
+          <button onClick={this.props.logout}>Log Out</button>
         </div>
       </div>
     ) : (
@@ -42,7 +40,6 @@ class NavBar extends React.Component {
         </div>
         <div className="search-nav">
           <SearchBarContainer />
-        
         </div>
         <div className="right-nav">
           <div className="nav-2">{navDisplay}</div>
