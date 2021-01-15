967ded6a777de5b1c2ed2f5d988cb29f5d186dd1
Tony <chentony17@gmail.com>
2021-01-15 15:57:44 -0500

    update splash

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -4,7 +4,7 @@ import LoginFormContainer from "./session_form/login_form_container";
 import NavBar from "./navbar/navbar_container";
 import SignUpContainer from "./session_form/signup_form_container";
 import { AuthRoute, Protected } from "../util/route_util";
-import SplashPage from "./splash/splash_page";
+import Splash from "./splash/splash_page";
 import Modal from "./modal/modal";
 import RideIndex from "./rides/ride_index_container";
 import RideShowContainer from "./rides/ride_show_container";
@@ -15,7 +15,7 @@ const App = () => (
     <Modal />
     <Route path="/" component={NavBar} />
     <Switch>
-      <Route exact path="/" component={SplashPage} />
+      <Route exact path="/" component={Splash} />
       <Route exact path="/rides" component={RideIndex} />
       {/* <Route exact path="/rides" component={Ride} /> */}
       <Route exact path="/rides/:rideId" component={RideShowContainer} />
--- frontend/components/splash/splash_page.jsx
+++ frontend/components/splash/splash_page.jsx
@@ -1,123 +1,133 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-export default function splash_page() {
-  return (
-    <div className="splashpage">
-      <div className="banner">
-        <div className="header-content">
-          <h1 className="welcome">Find The Perfect Ride</h1>
-        </div>
-        <div width="100%" height="50%">
-          <img
-            autoPlay
-            muted
-            loop
-            className="splash-image"
-            src={window.splashvid}
-          ></img>
-        </div>
-      </div>
-      <div className="look-btn">
-        <Link className="look-button" to="/rides">
-          Look Around
-        </Link>
-      </div>
-      <div className="riding-style">
-        <div className="style-col">
-          <img src={window.sport} className="style-img"></img>
-          <span className="style-title">Sport</span>
-          <div className="out-wrap">
-            <span className="style-link">Like the power and speed?</span>
+class Splash extends React.Component {
+  constructor(props) {{
+    super(props);
+  }}
+  
+  
+  render() {
+    return (
+      <div className="splashpage">
+        <div className="banner">
+          <div className="header-content">
+            <h1 className="welcome">Find The Perfect Ride</h1>
+          </div>
+          <div width="100%" height="50%">
+            <img
+              autoPlay
+              muted
+              loop
+              className="splash-image"
+              src={window.splashvid}
+            ></img>
           </div>
         </div>
-        <div className="style-col">
-          <img src={window.street} className="style-img"></img>
-          <span className="style-title">Street</span>
-          <div className="out-wrap">
-            <span className="style-link">
-              Like to commute or cruise the city?
-            </span>
+        <div className="look-btn">
+          <Link className="look-button" to="/rides">
+            Look Around
+          </Link>
+        </div>
+        <div className="riding-style">
+          <div className="style-col">
+            <img src={window.sport} className="style-img"></img>
+            <span className="style-title">Sport</span>
+            <div className="out-wrap">
+              <span className="style-link">Like the power and speed?</span>
+            </div>
           </div>
-        </div>
-
-        <div className="style-col">
-          <img src={window.touring} className="style-img"></img>
-          <span className="style-title">Adventure/Touring</span>
-          <div className="out-wrap">
-            <span className="style-link">Looking for a long ride?</span>
-          </div>
-        </div>
-
-        <div className="style-col">
-          <img src={window.dirt} className="style-img"></img>
-          <span className="style-title">Dirt</span>
-          <div className="out-wrap">
-            <span className="style-link">Looking to offroad?</span>
-          </div>
-        </div>
-      </div>
-
-      <div className="splash-exp">
-        <div className="header-text">
-          <span className="exp-header">Experiences</span>
-          <p className="exp-para">
-            Meet People around the city, experiencing different sceneries
-            together or different type of riding with other riders!
-          </p>
-        </div>
-
-        <div className="splash-exp-area">
-          <div className="parent-box1">
-            <img src={window.track1} className="big-pic"></img>
-            <div className="parent-desc-box1">
-              <span className="exp-desc">
-                In it for the adrenaline? Join the track experience.
+          <div className="style-col">
+            <img src={window.street} className="style-img"></img>
+            <span className="style-title">Street</span>
+            <div className="out-wrap">
+              <span className="style-link">
+                Like to commute or cruise the city?
               </span>
             </div>
           </div>
 
-          <div className="topgrid">
-            <div className="parent-box2-container">
-              <div className="parent-box2">
-                <img src={window.group} className="side-images"></img>
-                <div className="parent-desc-box">
-                  <span className="exp-desc">
-                    Checkout nearby group meetups! Meet new riders.
-                  </span>
-                </div>
-              </div>
+          <div className="style-col">
+            <img src={window.touring} className="style-img"></img>
+            <span className="style-title">Adventure/Touring</span>
+            <div className="out-wrap">
+              <span className="style-link">Looking for a long ride?</span>
+            </div>
+          </div>
 
-              <div className="parent-box2">
-                <img src={window.learn} className="side-images"></img>
-                <div className="parent-desc-box">
-                  <span className="exp-desc">
-                    Take care of your bike, join a maintenance class!
-                  </span>
-                </div>
+          <div className="style-col">
+            <img src={window.dirt} className="style-img"></img>
+            <span className="style-title">Dirt</span>
+            <div className="out-wrap">
+              <span className="style-link">Looking to offroad?</span>
+            </div>
+          </div>
+        </div>
+
+        <div className="splash-exp">
+          <div className="header-text">
+            <span className="exp-header">Experiences</span>
+            <p className="exp-para">
+              Meet People around the city, experiencing different sceneries
+              together or different type of riding with other riders!
+            </p>
+          </div>
+
+          <div className="splash-exp-area">
+            <div className="parent-box1">
+              <img src={window.track1} className="big-pic"></img>
+              <div className="parent-desc-box1">
+                <span className="exp-desc">
+                  In it for the adrenaline? Join the track experience.
+                </span>
               </div>
             </div>
-            <div className="parent-box2-container">
-              <div className="parent-box2">
-                <img src={window.offroad} className="side-images"></img>
-                <div className="parent-desc-box">
-                  <span className="exp-desc">
-                    Like the unpredictable? Try motorcrossing events!
-                  </span>
+
+            <div className="topgrid">
+              <div className="parent-box2-container">
+                <div className="parent-box2">
+                  <img src={window.group} className="side-images"></img>
+                  <div className="parent-desc-box">
+                    <span className="exp-desc">
+                      Checkout nearby group meetups! Meet new riders.
+                    </span>
+                  </div>
+                </div>
+
+                <div className="parent-box2">
+                  <img src={window.learn} className="side-images"></img>
+                  <div className="parent-desc-box">
+                    <span className="exp-desc">
+                      Take care of your bike, join a maintenance class!
+                    </span>
+                  </div>
                 </div>
               </div>
-              <div className="parent-box2">
-                <img src={window.stunt} className="side-images"></img>
-                <div className="parent-desc-box">
-                  <span className="exp-desc">
-                    Experience stunt shows or learn it from experienced riders.
-                  </span>
+              <div className="parent-box2-container">
+                <div className="parent-box2">
+                  <img src={window.offroad} className="side-images"></img>
+                  <div className="parent-desc-box">
+                    <span className="exp-desc">
+                      Like the unpredictable? Try motorcrossing events!
+                    </span>
+                  </div>
+                </div>
+                <div className="parent-box2">
+                  <img src={window.stunt} className="side-images"></img>
+                  <div className="parent-desc-box">
+                    <span className="exp-desc">
+                      Experience stunt shows or learn it from experienced
+                      riders.
+                    </span>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
+
+export default Splash;
