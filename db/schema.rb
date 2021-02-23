f2c0e563876d488da65612fb71f35752e0279d16
Tony <chentony17@gmail.com>
2021-02-22 21:52:51 -0500

    migration type change

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_14_043338) do
+ActiveRecord::Schema.define(version: 2021_02_23_024953) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,8 +40,8 @@ ActiveRecord::Schema.define(version: 2021_02_14_043338) do
     t.integer "ride_id", null: false
     t.integer "rider_id", null: false
     t.integer "num_riders", null: false
-    t.datetime "start_date", null: false
-    t.datetime "end_date", null: false
+    t.date "start_date", null: false
+    t.date "end_date", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
