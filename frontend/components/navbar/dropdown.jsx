65c36cca77d482ebc179a33d6f7658e50025bc84
Tony <chentony17@gmail.com>
2021-10-08 18:56:54 -0400

    add drop down

--- frontend/components/navbar/dropdown.jsx
+++ frontend/components/navbar/dropdown.jsx
@@ -1,2 +1,26 @@
-import React from 'react';
-import LoginDrop from './login_drop';
+import React from "react";
+import LoginDrop from "./login_drop";
+import LogoutDrop from "./logout_drop";
+
+class DropDown extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { menu: false };
+    this.toggleMenu = this.toggleMenu.bind(this);
+  }
+  componentWillUnmount() {
+    document.removeEventListener("click", this.toggleMenu);
+  }
+
+  toggleMenu(e) {
+    e.preventDefault();
+    e.stopPropgagation();
+    this.setState({ menu: !this.state.menu }, () => {
+      if (this.state.menu) {
+        document.addEventListener("click", this.toggleMenu);
+      } else {
+        document.removeEventListener("click", this.toggleMenu);
+      }
+    });
+  }
+}
